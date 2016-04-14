0cdfe0159264727c6df19aeca426c9520684ba73
NIGELSMK <kahler.maynard@gmail.com>
2016-04-13 20:30:02 -0400

    google maps api works gud

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,7 +1,7 @@
 angular.module('app', ['uiGmapgoogle-maps'])
     .config(function(uiGmapGoogleMapApiProvider) {
         uiGmapGoogleMapApiProvider.configure({
-            key: 'AIzaSyAfaIHSWkDyduGQlTxa-GGLm53owf9OyH0',
+            key: 'AIzaSyDmY2IlkIcBb92ol1jJZzhyPzFDPTtqBBw',
             v: '3.22',
             libraries: 'weather,geometry,visualization'
         });
