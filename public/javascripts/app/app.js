1728c9dff3ea70b732b4328cd55664576d273f66
NIGELSMK <kahler.maynard@gmail.com>
2016-04-14 23:44:26 -0400

    merged and linked up with Chris' html and css

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,7 +1,7 @@
 angular.module('app', ['uiGmapgoogle-maps'])
     .config(function(uiGmapGoogleMapApiProvider) {
         uiGmapGoogleMapApiProvider.configure({
-            "key": "5I4NW1kkpujJCiF0JKWdYO-alymgQ_AIzaSyAhqn",
+            "key": "AIzaSyAhqn5I4NW1kkpujJCiF0JKWdYO-alymgQ",
             "v": "3.22",
             "libraries": "weather,geometry,visualization"
         });
--- public/javascripts/app/mapCtrl.js
+++ public/javascripts/app/mapCtrl.js
@@ -1,6 +1,12 @@
 angular.module('app').controller("MapCtrl", function($scope, uiGmapGoogleMapApi, RestaurantsDataSvc) {
     //define relevant variables
     //@43.6690522,-79.395342,17z
+    $scope.homeMarker = {
+        coords: { latitude: 43.669211, longitude: -79.393164 },
+        options: {
+            icon: "http://static.wixstatic.com/media/c7ec87_091b4494cc4b49418a847caaba6a4e1b.png_srz_35_35_85_22_0.50_1.20_0.00_png_srz"
+        }
+    }
     $scope.map = { center: { latitude: 43.6690522, longitude: -79.395342 }, zoom: 18 };
     $scope.options = { scrollwheel: false };
     uiGmapGoogleMapApi.then(function(maps) {
